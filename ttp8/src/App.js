adbabd3048b3a931292d3df775a68174fae58817
Hesham <heshamsharafaldin@gmail.com>
2023-06-22 00:05:49 -0400

    Gif search results work but with some bugs

--- ttp8/src/App.js
+++ ttp8/src/App.js
@@ -1,33 +1,77 @@
 import "./styles.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Search from "./components/Search";
 import Results from "./components/GifResults";
 
 function App() {
-  const [newItem, setNewItem] = useState("");
+  const [trending, setTrending] = useState([]);
+  const [Search, setSearch] = useState([]);
+  const [random, setRandom] = useState({});
 
-  const handleSubmit = (event) => {
+  useEffect(() => {
+    async function fetchTrendingGifs() {
+      try {
+        const list = await axios.get(
+          "http://api.giphy.com/v1/gifs/trending?api_key=aoOHTOv4THJJ4XDSBcfFle0JWoJCu5Dc"
+        );
+        setTrending(list.data.data);
+        setSearch([]);
+        setRandom({});
+        console.log(list);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchTrendingGifs();
+  }, []);
+
+  //Fetches gifs based on search
+  async function fetchSearchGifs(search) {
+    try {
+      const gifData = await axios.get(
+        "http://api.giphy.com/v1/gifs/search?q=" +
+          search +
+          "&api_key=aoOHTOv4THJJ4XDSBcfFle0JWoJCu5Dc"
+      );
+      const arr = gifData.data.data;
+      console.log("fetching...", arr);
+      setSearch(arr);
+      setTrending([]);
+      setRandom({});
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleInput = (event) => {
     event.preventDefault();
-    setNewItem(event.target.value);
-    console.log(newItem);
-    //Search(newItem);
+    fetchSearchGifs(Search);
+    console.log("handleInput:", Search);
+  };
+
+  const handleInputChange = (event) => {
+    setSearch(event.target.value);
+    console.log("handleinputChange: ", event.target.value);
   };
 
   return (
     <>
-      <form className="new-item-form" onSubmit={handleSubmit}>
+      <form className="new-item-form" onSubmit={handleInput}>
         <div className="form-row">
           <label htmlFor="item"> Search Gifs </label>
           <input
             type="text"
             placeholder="GIFY!"
-            value={newItem ?? ""}
-            onChange={(event) => setNewItem(event.target.value)}
             id="item"
+            value={Search ?? ""}
+            onChange={handleInputChange}
           />
+          <button className="btn">Search</button>
         </div>
-        <button className="btn">Search</button>
       </form>
+
+      <Results data={Search}/>
     </>
   );
 }
--- ttp8/src/components/GifResults.jsx
+++ ttp8/src/components/GifResults.jsx
@@ -1,19 +1,26 @@
-
-
 const Results = (props) => {
+    
+    const { data } = props;
+    
     return (
-        <>
-            {props.data.map((gif) => {
-                return (
-                    <div key = {gif.id}>
-                        <img id="display" src= {gif.images.fixed_height.url} alt = "gif" autoplay loop inline="true"></img>
-                    </div>
-                );
-            }
-         )}
-        </>
-        
-    );
-}
+    <>
+      {Array.isArray(props.data) ? (
+        data.map((gif) => (
+          <img
+            id="display"
+            key={gif.id}
+            src={gif.images.fixed_height.url}
+            alt="gif"
+            autoPlay
+            loop
+            inline="true"
+          />
+        ))
+      ) : (
+        <p>No results found.</p>
+      )}
+    </>
+  );
+};
 
-export default Results
+export default Results;
